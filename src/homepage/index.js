239a8bfc382e933d2a51b3d9dd49eefd8582b0e8
gerCasas <gcasasg4@gmail.com>
2017-07-20 16:36:01 -0700

    23 - Agregando la funcionalidad de like

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -25,6 +25,24 @@ page('/', function (ctx, next) {
       url: 'http://materializecss.com/images/sample-1.jpg',
       likes: 234,
       liked: false
+    },
+    {
+      user: {
+        username: 'Luis',
+        avatar: 'https://lh3.googleusercontent.com/XovXLpWy6M4ggoucnWNge4hgrjAsX22_hT2WDg_hE0e-FNScEnKr8z6J479jvnhjW8_gyu6U6Y4NlMx3cg=w5120-h3200-rw-no'
+      },
+      url: 'http://materializecss.com/images/office.jpg',
+      likes: 123,
+      liked: true
+    },
+    {
+      user: {
+        username: 'German',
+        avatar: 'https://lh3.googleusercontent.com/4EibVauK5UIfrNteW8hp7tBREGtnhTSxdEhmLR8j5gE-d5JlqFw1H3BXfqkVKOwzqaNbvbmpPAj1Rl-c=w5120-h3200-rw-no'
+      },
+      url: 'http://materializecss.com/images/sample-1.jpg',
+      likes: 234,
+      liked: false
     }
   ];
 
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -6,11 +6,11 @@ var picture = require('../picture-card');
 module.exports = function (pictures) {
   var el = yo`<div class="container timeline">
     <div class="row">
-      <div class="col s12 m10 offset-m1 l6 offset-l3">
+
         ${pictures.map( function (pic) {
           return picture(pic);
         })}
-      </div>
+    
     </div>
   </div>`;
 
